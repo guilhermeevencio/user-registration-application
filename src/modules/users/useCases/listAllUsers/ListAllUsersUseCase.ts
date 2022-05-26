52668093fd5f5d940b1c2d2df7e496336b144628
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-26 16:15:03 -0300

    feat: list users.

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user.admin) {
+      throw new Error("Permition denied!");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
